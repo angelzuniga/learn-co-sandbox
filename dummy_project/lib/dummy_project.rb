ec451696c0906851cfd8b929e031adf9a2233227
Angel Zuniga <angelzuniga@outlook.com>
2021-04-28 06:12:11 +0000

    Automatically backed up by Learn

---
+++ dummy_project/lib/dummy_project.rb
@@ -0,0 +1,6 @@
+require "dummy_project/version"
+
+module DummyProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ dummy_project/lib/dummy_project/version.rb
@@ -0,0 +1,3 @@
+module DummyProject
+  VERSION = "0.1.0"
+end
